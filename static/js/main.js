bd83194834fd88813c19ff95e83c1fa7dc517270
Pachimarii <minhui.ivy.xie@gmail.com>
2020-02-26 18:39:09 -0500

    added top and side nav

---
+++ static/js/main.js
@@ -0,0 +1,75 @@
+// var categoricalFields = ["Creative Type","Director","Distributor","Major Genre","MPAA Rating","Source","Title"];
+// var dateFields = ["Release Date"];
+// var translatable_numericFields = ["IMDB Rating","IMDB Votes", "Production Budget","Rotten Tomatoes", "Running Time min","US DVD Sales","US Gross","Worldwide Gross"];
+// var numericFields = ["COUNT"];
+
+var Fields = {
+    cate: ["Creative Type","Director","Distributor","Major Genre","MPAA Rating","Source","Title"],
+    date: ["Release Date"],
+    trans_quant: ["IMDB Rating","IMDB Votes", "Production Budget","Rotten Tomatoes", "Running Time min","US DVD Sales","US Gross","Worldwide Gross"],
+    quant: ["COUNT"]
+};
+var fieldLst = document.querySelector(".attr-lst");
+
+// a helper function that put all attrbutes to field list on sidenav.
+function addFields(fields){
+    let res = "";
+    for (e of fields.cate) {
+        res+=`<li class="cate-attr">
+                <div>
+                    <i class="fas fa-font"></i> &nbsp;
+                    <span> ${e} </span>
+                    <span class="float-right">
+                        <i class="fas fa-filter"></i>
+                        <i class="fas fa-plus"></i></i>
+                    </span>
+                </div>
+            </li>`;
+    }
+    for (e of fields.date) {
+        res+=`<li class="cate-attr">
+                <div>
+                    <i class="fas fa-calendar-alt"></i> &nbsp;
+                    <span> ${e} </span>
+                    <span class="float-right">
+                        <i class="fas fa-filter"></i>
+                        <i class="fas fa-plus"></i></i>
+                    </span>
+                </div>
+            </li>`;
+    }
+    for (e of fields.trans_quant) {
+        res+=`<li class="trans-quant-attr">
+                <div>
+                    <i class="fas fa-caret-down"></i> &nbsp;
+                    <i class="fas fa-hashtag"></i> &nbsp;
+                    <span> IMDB Ratings</span>
+                    <span class="float-right">
+                        <i class="fas fa-filter"></i>
+                        <i class="fas fa-plus"></i></i>
+                    </span>
+                </div>
+            </li>`;
+    }
+    for (e of fields.quant) {
+        res+=`<li class="quant-attr">
+                <div>
+                    <i class="fas fa-hashtag"></i> &nbsp;
+                    <span> COUNT</span>
+                    <span class="float-right">
+                        <i class="fas fa-filter"></i>
+                        <i class="fas fa-plus"></i></i>
+                    </span>
+                </div>
+            </li>`;
+    }
+    fieldLst.innerHTML+=res;
+}
+
+// does what it said.. it init the field list on sidenav.
+function initField(fields){
+    fieldLst.innerHTML='';
+    addFields(fields);
+};
+
+initField(Fields);
